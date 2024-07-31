8f0ca78be24ed570a621cec7090f2e569a19e416
veeresh-27 <veereshrm789@gmail.com>
2024-07-31 22:55:09 +0530

    instagram Icon import

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,7 +1,7 @@
-import InstagramIcon from "../../assets/icons/svgs/InstagramIcon.svg";
 import GithubIcon from "../../assets/icons/svgs/GithubIcon.svg";
 import LinkedInIcon from "../../assets/icons/svgs/LinkedinIcon.svg";
 import TwitterIcon from "../../assets/icons/svgs/XIcon.svg";
 import HashNodeIcon from "../../assets/icons/svgs/HashnodeIcon.svg";
+import InstagramIcon from "../../assets/icons/svgs/InstagramIcon.svg";
 
 export { InstagramIcon, GithubIcon, LinkedInIcon, TwitterIcon, HashNodeIcon };
