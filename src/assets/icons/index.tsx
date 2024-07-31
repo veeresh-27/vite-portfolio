441cb832b96cdf9f97aa211e6e05cf468972bcbe
veeresh-27 <veereshrm789@gmail.com>
2024-07-31 22:57:58 +0530

    Pusing Mannualu

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -2,6 +2,6 @@ import GithubIcon from "../../assets/icons/svgs/GithubIcon.svg";
 import LinkedInIcon from "../../assets/icons/svgs/LinkedinIcon.svg";
 import TwitterIcon from "../../assets/icons/svgs/XIcon.svg";
 import HashNodeIcon from "../../assets/icons/svgs/HashnodeIcon.svg";
-import InstagramIcon from "../../assets/icons/svgs/InstagramIcon.svg";
+import InstagramIcon from "../../assets/icons/svgs/InstaIcon.svg";
 
 export { InstagramIcon, GithubIcon, LinkedInIcon, TwitterIcon, HashNodeIcon };
